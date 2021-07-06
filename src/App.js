becfc1d43d3485cad7fb67c8ca6b342499f4a268
Tatiana-goit <tanchou74@gmail.com>
2021-07-06 14:26:33 +0300

    task 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Counter from './components/Counter/Counter';
+import Section from './components/Section/Section';
+
+function App() {
+  return (
+    <div className="App">
+      <Section title='Example 1'><Counter/> </Section>
+
+      <Section title='Example 2'> </Section>
+
+      <Section title='Example 3'> </Section>
+      
+    </div>
+  );
+}
+
+export default App;
