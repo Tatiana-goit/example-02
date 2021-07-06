70d1ee816b77d5e0056bb4c0e473f321c769976f
Tatiana-goit <tanchou74@gmail.com>
2021-07-06 16:42:38 +0300

    task 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Counter from './components/Counter/Counter';
 import Section from './components/Section/Section';
+import Dropdown from './components/Dropdown/Dropdown';
 
 function App() {
   return (
     <div className="App">
       <Section title='Example 1'><Counter/> </Section>
 
-      <Section title='Example 2'> </Section>
+      <Section title='Example 2'><Dropdown/> </Section>
 
       <Section title='Example 3'> </Section>
       
