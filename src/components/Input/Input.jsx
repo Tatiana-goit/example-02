8289bd79641ee5bd408c19a5d1a0d70c91da061d
Tatiana-goit <tanchou74@gmail.com>
2021-07-07 23:39:37 +0300

    input 3

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,49 +1,17 @@
 import {Component} from 'react';
+import Form from './Form'
 
 class Input extends Component {
-    state = {
-        name: '',
-        tag: '',
-    };
+state ={};
 
-    // handleChange = e => {
-    //     const {name,value} = e.cuttentTarget;
-    //     this.setState({
-    //         [name]: value})
-    // }
+formSubmitHandler = data => {
+    console.log(data);
+}
 
-    handleNameChange = event => {
-           this.setState({name: event.currentTarget.value})
-    }
-
-    handleTagChange = event => {
-        this.setState({tag: event.currentTarget.value})
-    }
-
-    render() {
-        return (
-            <form>
-                <label>
-                    Имя 
-                    <input 
-                        type='text' 
-                        name='name'
-                        value={this.state.name}
-                        onChange={this.handleNameChange}/>
-                </label>
-                <label>
-                    Прозвище 
-                    <input 
-                        type='text'
-                        name='tag'
-                        value={this.state.tag}
-                        onChange={this.handleTagChange}/>
-                </label>
-            </form>
-            // <input type="text" 
-            // value={this.state.inputValue}
-            // onChange={this.handleInputChange}/>
-        )
+render() {
+        return( <Form onSubmit={this.formSubmitHandler}></Form>)
+       
+       
     }
 }
 
