ab8c9a889684752a621b400ea1bbb159c984ff8b
Tatiana-goit <tanchou74@gmail.com>
2021-10-05 21:37:20 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Section from './components/Section/Section';
 import Dropdown from './components/Dropdown/Dropdown';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 import Input from './components/Input/Input'
+import Example1 from './components/Example1/Example1'
+import Example2 from './components/Example1/Example2'
 import ColorPickerOptions from './data/colorPickerOptions.json';
 
 
@@ -18,6 +20,11 @@ const App =() => {
         <Section title='Example 3'><ColorPicker options={ColorPickerOptions}/> </Section>
   
         <Section title='Example 4'><Input/> </Section>
+
+        <Section title='Example 5'><Example1/> </Section>
+
+        <Section title='Example 6'><Example2/> </Section>
+        
         
       </div>
     );
---
+++ src/components/Example1/Example1.jsx
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import Counter from './Counter';
+
+class New extends Component {
+    state = {
+        counter: 0,
+    }
+
+    handleDecrement = () => {
+        // this.setState({
+        //     counter: this.state.counter - 1,
+        // })
+        this.setState((prevState)=>({
+           counter: prevState.counter - 1,
+            
+        }))
+    }
+
+    handleIncrement = () => {
+        // this.setState({
+        //     counter: this.state.counter + 1,
+        // })
+        this.setState((prevState)=>({
+            counter: prevState.counter + 1,
+             
+         }))
+    }
+
+    render() {
+        const {counter} = this.state
+        const { handleDecrement,handleIncrement } = this
+        return (
+            <div>
+                <h1> My work</h1>
+                <Counter
+                    counter={counter}
+                    handleDecrement={handleDecrement}
+                    handleIncrement={handleIncrement}
+
+                />
+                {/* <div className='counter'>
+                    <button onClick={handleDecrement}>-</button>
+                    <p>{counter}</p>
+                    <button onClick={handleIncrement}>+</button>
+                </div> */}
+            </div>
+        )
+    }
+}
+
+export default New;
